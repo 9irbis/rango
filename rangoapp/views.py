40a65c7d6a7ba1e17a40191c475a3a2398fa9bcf
Nakul <9irbis@gmail.com>
2016-02-06 01:04:10 +0530

    implemented functionality for adding new categories and pages

--- rangoapp/views.py
+++ rangoapp/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from rangoapp.models import Category, Page
+from rangoapp.forms import CategoryForm, PageForm
+from django.http import HttpResponse
 
 
 def index(request):
@@ -21,6 +23,42 @@ def category(request, category_name_slug):
         pages = Page.objects.filter(category = category)
         context_dict['pages'] = pages
         context_dict['category'] = category
+        context_dict['category_name_slug'] = category_name_slug
     except Category.DoesNotExist:
         pass
     return render(request, 'rangoapp/category.html', context_dict)
+
+
+def add_category(request):
+    if request.method == 'POST':
+        form = CategoryForm(request.POST)
+        if form.is_valid():
+            form.save(commit=True)
+            return index(request)
+        else:
+            print(form.errors)
+    else:
+        form = CategoryForm()
+    return render(request, 'rangoapp/add_category.html', {'form': form})
+
+
+def add_page(request, category_name_slug):
+    try:
+        cat = Category.objects.get(slug=category_name_slug)
+    except Category.DoesNotExist:
+        cat = None
+
+    if request.method == 'POST':
+        form = PageForm(request.POST)
+        if form.is_valid():
+            if cat:
+                instance = form.save(commit=False)
+                instance.category = cat
+                instance.save()
+                return category(request, category_name_slug)
+        else:
+            print(form.errors)
+    else:
+        form = PageForm()
+    context_dict = {'form': form, 'category': cat}
+    return render(request, 'rangoapp/add_page.html', context_dict)
