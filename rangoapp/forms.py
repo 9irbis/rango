31eae6d1d17bf56f768d366c361f0c560588fe02
Nakul <9irbis@gmail.com>
2016-02-10 00:32:09 +0530

    Improved presentation with bootstrap

--- rangoapp/forms.py
+++ rangoapp/forms.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 
 class CategoryForm(forms.ModelForm):
-    name = forms.CharField(max_length=128, help_text='Enter the category name.')
+    name = forms.CharField(max_length=128, help_text='Enter the category name:')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     likes = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
     slug = forms.CharField(widget=forms.HiddenInput(), required=False)
@@ -15,8 +15,8 @@ class CategoryForm(forms.ModelForm):
 
 
 class PageForm(forms.ModelForm):
-    title = forms.CharField(max_length=128, help_text='Enter the page title here.')
-    url = forms.URLField(max_length=200, help_text='Enter the page url here.')
+    title = forms.CharField(max_length=128, help_text='Enter the page title here:')
+    url = forms.URLField(max_length=200, help_text='Enter the page url here:')
     views = forms.IntegerField(widget=forms.HiddenInput(), initial=0)
 
     class Meta:
